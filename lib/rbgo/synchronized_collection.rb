72baba67089008610e1c103ce9ed59bae159f862
wangyin <24588062@qq.com>
2019-09-26 01:28:37 +0800

    add Sync module

--- lib/rbgo/synchronized_collection.rb
+++ lib/rbgo/synchronized_collection.rb
@@ -2,72 +2,74 @@ require 'monitor'
 require 'set'
 
 module Rbgo
-  class Array
-    def self.[](*args)
-      Array.new(::Array.[](*args))
-    end
+  module Sync
+    class Array
+      def self.[](*args)
+        Array.new(::Array.[](*args))
+      end
 
-    def self.try_convert(obj)
-      a = ::Array.try_convert(obj)
-      a.nil? ? nil : Array.new(a)
-    end
+      def self.try_convert(obj)
+        a = ::Array.try_convert(obj)
+        a.nil? ? nil : Array.new(a)
+      end
 
-    def initialize(*args, &blk)
-      @a = ::Array.new(*args, &blk)
-      @a.extend(MonitorMixin)
-    end
+      def initialize(*args, &blk)
+        @a = ::Array.new(*args, &blk)
+        @a.extend(MonitorMixin)
+      end
 
-    ::Array.public_instance_methods.each do |m|
-      define_method(m) do |*args, &blk|
-        @a.synchronize do
-          @a.send m, *args, &blk
+      ::Array.public_instance_methods.each do |m|
+        define_method(m) do |*args, &blk|
+          @a.synchronize do
+            @a.send m, *args, &blk
+          end
         end
       end
     end
-  end
 
-  class Hash
-    def self.[](*args)
-      h = Hash.new
-      h.instance_eval do
-        @h = ::Hash.[](*args)
+    class Hash
+      def self.[](*args)
+        h = Hash.new
+        h.instance_eval do
+          @h = ::Hash.[](*args)
+          @h.extend(MonitorMixin)
+        end
+      end
+
+      def self.try_convert(obj)
+        h = ::Hash.try_convert(obj)
+        h.nil? ? nil : Hash.[](h)
+      end
+
+      def initialize(*args, &blk)
+        @h = ::Hash.new(*args, &blk)
         @h.extend(MonitorMixin)
       end
-    end
 
-    def self.try_convert(obj)
-      h = ::Hash.try_convert(obj)
-      h.nil? ? nil : Hash.[](h)
-    end
-
-    def initialize(*args, &blk)
-      @h = ::Hash.new(*args, &blk)
-      @h.extend(MonitorMixin)
-    end
-
-    ::Hash.public_instance_methods.each do |m|
-      define_method(m) do |*args, &blk|
-        @h.synchronize do
-          @h.send m, *args, &blk
+      ::Hash.public_instance_methods.each do |m|
+        define_method(m) do |*args, &blk|
+          @h.synchronize do
+            @h.send m, *args, &blk
+          end
         end
       end
     end
-  end
 
-  class Set
-    def self.[](*args)
-      Set.new(::Set.[](*args))
-    end
+    class Set
+      def self.[](*args)
+        Set.new(::Set.[](*args))
+      end
 
-    def initialize(enum = nil)
-      @s = ::Set.new(enum)
-      @s.extend(MonitorMixin)
-    end
+      def initialize(enum = nil)
+        @s = ::Set.new(enum)
+        @s.extend(MonitorMixin)
+      end
 
-    ::Set.public_instance_methods.each do |m|
-      define_method(m) do |*args, &blk|
-        @s.synchronize do
-          @s.send m, *args, &blk
+      ::Set.public_instance_methods.each do |m|
+        define_method(m) do |*args, &blk|
+          @s.synchronize do
+            @s.send m, *args, &blk
+          end
         end
       end
     end
