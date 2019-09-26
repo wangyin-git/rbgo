ec62e0de45ba25ea25b7c264a0c71b472486719c
wangyin <24588062@qq.com>
2019-09-26 14:57:33 +0800

    add link/monitor, parent-child relationship to actor

--- lib/rbgo/actor.rb
+++ lib/rbgo/actor.rb
@@ -1,36 +1,169 @@
 require 'thread'
 require_relative 'once'
+require_relative 'synchronized_collection'
+require_relative 'reentrant_mutex'
 
 module Rbgo
+  class ActorCloseMsg
+    attr_accessor :actor
+
+    def initialize(actor)
+      self.actor = actor
+    end
+  end
+
   class Actor
     private
 
-    attr_accessor :mail_box, :once_for_msg_loop
+    attr_accessor :mail_box, :once_for_msg_loop, :once_for_close,
+                  :close_mutex, :parent, :children, :linked_actors, :supervisor_actors
 
     public
 
     attr_accessor :handler
+    attr_reader :close_reason
 
     def initialize(&handler)
       self.handler           = handler
+      @close_reason          = nil
       self.mail_box          = Queue.new
       self.once_for_msg_loop = Once.new
+      self.once_for_close    = Once.new
+      self.close_mutex       = ReentrantMutex.new
+
+      self.children          = SyncSet.new
+      self.linked_actors     = SyncSet.new
+      self.supervisor_actors = SyncSet.new
     end
 
     def send_msg(msg)
       mail_box << msg
       start_msg_loop
-      nil
+      self
     end
 
-    def close
-      mail_box.close
-      mail_box.clear
-      nil
+    def send_children(msg)
+      children.each do |child|
+        child.send_msg(msg) rescue nil
+      end
+      self
+    end
+
+    def close(reason = nil)
+      once_for_close.do do
+        close_mutex.synchronize do
+          @close_reason = reason
+          mail_box.close
+          mail_box.clear
+
+          (parent&.send :children)&.delete(self)
+          self.parent   = nil
+          self.children = nil
+
+          linked_actors.each do |l|
+            CoRun::Routine.new(new_thread: false, queue_tag: :default) do
+              l.close
+            end
+          end
+          self.linked_actors = nil
+
+          supervisor_actors.each do |sup|
+            sup.send_msg(ActorCloseMsg.new(self)) rescue nil
+          end
+          self.supervisor_actors = nil
+
+          nil
+        end
+      end
     end
 
     def closed?
       mail_box.closed?
+    end
+
+    def spawn_child(&handler)
+      close_mutex.synchronize do
+        raise "can not spawn from a closed actor" if closed?
+        child = Actor.new(&handler)
+        child.send :parent=, self
+        children.add(child)
+        child
+      end
+    end
+
+    def spawn_link(&handler)
+      close_mutex.synchronize do
+        raise "can not spawn from a closed actor" if closed?
+        l = spawn_child(&handler)
+        l.send(:linked_actors).add(self)
+        linked_actors.add(l)
+        l
+      end
+    end
+
+    def link(actor)
+      return self if self.equal?(actor)
+      close_mutex.synchronize do
+        raise "can not link from a closed actor" if closed?
+        actor.send(:close_mutex).synchronize do
+          if actor.closed?
+            close
+          else
+            actor.send(:linked_actors).add(self)
+            linked_actors.add(actor)
+          end
+        end
+        self
+      end
+    end
+
+    def unlink(actor)
+      return self if self.equal?(actor)
+      close_mutex.synchronize do
+        raise "can not unlink from a closed actor" if closed?
+        actor.send(:close_mutex).synchronize do
+          unless actor.closed?
+            actor.send(:linked_actors).delete(self)
+          end
+          linked_actors.delete(actor)
+        end
+        self
+      end
+    end
+
+    def spawn_monitor(&handler)
+      close_mutex.synchronize do
+        raise "can not spawn from a closed actor" if closed?
+        m = spawn_child(&handler)
+        m.send(:supervisor_actors).add(self)
+        m
+      end
+    end
+
+    def monitor(actor)
+      return self if self.equal?(actor)
+      close_mutex.synchronize do
+        raise "can not monitor from a closed actor" if closed?
+        actor.send(:close_mutex).synchronize do
+          if actor.closed?
+            send_msg(ActorCloseMsg.new(actor))
+          else
+            actor.send(:supervisor_actors).add(self)
+          end
+        end
+        self
+      end
+    end
+
+    def demonitor(actor)
+      return self if self.equal?(actor)
+      close_mutex.synchronize do
+        raise "can not demonitor from a closed actor" if closed?
+        actor.send(:close_mutex).synchronize do
+          actor.send(:supervisor_actors)&.delete(self)
+        end
+        self
+      end
     end
 
     private
@@ -57,6 +190,7 @@ module Rbgo
     def call_handler(msg)
       handler.call(msg, self) if handler
     rescue Exception => ex
+      close(ex)
       STDERR.puts ex
     end
   end
