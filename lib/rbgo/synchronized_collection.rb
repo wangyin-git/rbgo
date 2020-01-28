de5df3a747bdf1c05358e074dce1a55e2fa6f18d
wangyin <24588062@qq.com>
2020-01-28 17:44:30 +0800

    sync collections

--- lib/rbgo/synchronized_collection.rb
+++ lib/rbgo/synchronized_collection.rb
@@ -3,43 +3,28 @@ require 'set'
 
 module Rbgo
   class SyncArray < Array
-    include MonitorMixin
-
-    def initialize(*args)
-      super(*args)
-    end
-
-    Array.public_methods.each do |m|
+    @mutex = Mutex.new
+    Array.public_instance_methods.each do |m|
       define_method(m) do |*args, &blk|
-        synchronize { super(*args, &blk) }
+        SyncArray.instance_variable_get(:@mutex).synchronize { super(*args, &blk) }
       end
     end
   end
 
   class SyncHash < Hash
-    include MonitorMixin
-
-    def initialize(*args)
-      super(*args)
-    end
-
-    Hash.public_methods.each do |m|
+    @mutex = Mutex.new
+    Hash.public_instance_methods.each do |m|
       define_method(m) do |*args, &blk|
-        synchronize { super(*args, &blk) }
+        SyncHash.instance_variable_get(:@mutex).synchronize { super(*args, &blk) }
       end
     end
   end
 
   class SyncSet < Set
-    include MonitorMixin
-
-    def initialize(*args)
-      super(*args)
-    end
-
-    Set.public_methods.each do |m|
+    @mutex = Mutex.new
+    Set.public_instance_methods.each do |m|
       define_method(m) do |*args, &blk|
-        synchronize { super(*args, &blk) }
+        SyncSet.instance_variable_get(:@mutex).synchronize { super(*args, &blk) }
       end
     end
   end
